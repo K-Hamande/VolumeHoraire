18a47ae037f036cb6dd6d6f42ca99e56993a986f
Hamadé <hamandea@gmail.com>
2022-06-09 18:20:07 +0000

    Formation

--- resources/views/Enseignants/EnseignantActivite.blade.php
+++ resources/views/Enseignants/EnseignantActivite.blade.php
@@ -48,13 +48,13 @@
 
 
 
-
         <!--**********************************
             Content body start
         ***********************************-->
         <div class="content-body">
 
             <div class="container-fluid mt-3">
+
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card">
@@ -64,22 +64,51 @@
                                         <table class="table table-xs mb-0">
                                             <thead>
                                                 <tr>
-                                                    <th>Etablissement</th>
-                                                    <th>Departement</th>
-                                                    <th>Filière</th>
+                                                    <th>Matricule </th>
+                                                    <th>Nom</th>
+                                                    <th>Prénom(s)</th>
+                                                    <th>Grade</th>
                                                     <th>Action</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
                                                 <tr>
-                                                    <td>UFR/ST</td>
-                                                    <td>Mathematique</td>
-                                                    <td><span> Mathematique</span></td>
+                                                    <td><img src="./images/avatar/1.jpg" class=" rounded-circle mr-3" alt="">Sarah Smith</td>
+                                                    <td>iPhone X</td>
                                                     <td>
-                                                       <button class="btn btn-success"><a href="{{Route('ajoutFormation')}}"> Ajouter une formation </a></button>
-                                                        </div>
+                                                        <span>United States</span>
+                                                    </td>
+                                                    <td><i class="fa fa-circle-o text-success  mr-2"></i> Paid</td>
+                                                        <td> 
+                                                            <a href="#"><button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModalCenter">Notifier</button></a>
+                                                            <!-- Modal -->
+                                                            <div class="modal fade" id="exampleModalCenter">
+                                                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                                                    <div class="modal-content">
+                                                                        <div class="modal-header">
+                                                                            <h5 class="modal-title"> Notification</h5>
+                                                                            <button type="button" class="close" data-dismiss="modal"><span> &times;</span>
+                                                                            </button>
+                                                                        </div>
+                                                                        <div class="modal-body" style="color: red">
+                                                                            <form>
+                                                                                <div class="form-group">
+                                                                                    <label for="message-text" class="col-form-label">Message:</label>
+                                                                                    <textarea class="form-control" id="message-text"></textarea>
+                                                                                </div>
+                                                                            </form>
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="button" class="btn btn-info" data-dismiss="modal">Annuler</button>
+                                                                        <button type="submit" class="btn btn-danger">Envoyer</button>
+                                                                    </div>
+                                                                            </form>
+                                                                    </div>       
+                                                            </div>
+                                                            </div>
                                                     </td>
                                                 </tr>
+                                               
                                             </tbody>
                                         </table>
                                     </div>
@@ -87,11 +116,12 @@
                             </div>
                         </div>                        
                     </div>
-                </div> 
-                
+                </div>
+
+            
             </div>
-            <!-- #/ container -->
         </div>
+            <!-- #/ container -->
         <!--**********************************
             Content body end
         ***********************************-->
